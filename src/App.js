20a5a13c79605927594749c7df0d9b8102566e74
unknown <siamur01@gmail.com>
2021-09-19 21:33:53 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,64 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+
+      <Family></Family>
+      <Family></Family>
+
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+
+
     </div>
   );
+}
+
+function Person() {
+  const person = {
+    backgroundColor: 'skyblue',
+    border: '5px solid lightsalmon',
+    margin: '20px',
+    borderRadious: '20 px'
+  }
+  return (
+    <div style={person}>
+
+      <h1>Name: Sakib All Hasan</h1>
+      <h4>Profession:Cricketer</h4>
+    </div>
+
+  );
+}
+
+function Family() {
+  return (
+
+    <div style={{
+      backgroundColor: 'skyblue',
+      border: '8px solid red',
+      margin: '20px',
+      borderRadious: '20 px'
+    }}>
+      <h1>wife: Shishir</h1>
+      <h4>child:3</h4>
+    </div>
+  );
+}
+
+
+
+function Friend() {
+  return (
+    <div className="person">
+      <h3>
+        Phone:
+      </h3>
+    </div>
+  )
 }
 
 export default App;
